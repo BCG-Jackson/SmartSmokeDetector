33ecef2b408509efc406beb91a0eeaf521200d3e
Jared Murphy <murphy.248@wright.edu>
2019-11-06 14:20:11 -0500

    Updated Prototype
    Changed smoke detecting and carbon detecting thresholds & sounding criteria.

--- prototypeThree.ino
+++ prototypeThree.ino
@@ -1,3 +1,6 @@
+#include <Time.h>
+#include <TimeLib.h>
+
 //1. Actuators (Line 6)
 //2. Sensors (Line 43)
 //3. Controller
@@ -6,20 +9,21 @@
 #include <Arduino.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <Time.h>
+//#include <Time.h>
+//#include <ctime>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
 #define MAXSMOKE 150
-#define MAXCARBON 150
-#define SNOOZETIME 250 //time in seconds
+#define MAXCARBON 0//800
+#define SNOOZETIME 300 //time in seconds, 5 minutes
 #define MAXREADINGS 32768
 
 #define SMOKEPIN A0
 #define COPIN A1
 #define SIRENPIN A2
 #define TEMPPIN 12
-#define LEDPIN 13
+#define LEDPIN 8
 
 //Global variables for the temperature sensor since it wouldn't work when placed in the TempSensor class
 OneWire oneWire(TEMPPIN);
@@ -35,12 +39,12 @@ class LEDController {
     }
 
     bool activate() {
-      analogWrite(sPin, 1023);
+      analogWrite(sPin, HIGH);
       return true;
     }
 
     bool deactivate() {
-      analogWrite(sPin, 0);
+      analogWrite(sPin, LOW);
       return true;
     }
 
@@ -153,21 +157,21 @@ class Controller {
     int readingsSaved;
     LEDController lc;
     SirenController sc;
-    time_t hushEnd = time(0);
+    time_t hushEnd = now();
 
-    bool lightsActivated;
+    bool lightsActivated = false;
+    bool sirenActivated = false;
     bool online;
 
     void getSensorData() { //Collect readings from the sensors.
-      Serial.println("Getting sensor data");
       currentReadings[0] = sd.getReading();
 
       //Code for the CO2 sensor reading
-      if (readingsSaved % 150 == 0) {
+      if (readingsSaved % (MAXREADINGS - 20) == 0) {
         currentReadings[1] = cd.getReading();
-      } else if (readingsSaved % 100 == 0) {
+      } else if (readingsSaved % (2* MAXREADINGS / 3) == 0) {
         cd.warmUpTwo();
-      } else if (readingsSaved % 50 == 0) {
+      } else if (readingsSaved % (MAXREADINGS / 3) == 0) {
         cd.warmUpOne();
       }
       currentReadings[2] = ts.getReading();
@@ -196,6 +200,7 @@ class Controller {
 
     void activateLights() { //Activate the lights
       lightsActivated = lc.activate();
+      Serial.print("Activating!");
     }
 
     void deactivateLights() { //Deactivate the lights
@@ -203,16 +208,17 @@ class Controller {
     }
 
     void activateAlarm() { //Activate the alarm
-      sc.activate();
+      sirenActivated = true;
     }
 
     void deactivateAlarm() { //Deactivate the alarm
-      sc.deactivate();
+      sirenActivated = false;
     }
 
     boolean isSnoozed() { //Checks to see if the the current time in milliseconds is larger than the end of the snooze period (HushEnd)
-      time_t currentTime = time(0);
+      time_t currentTime = now();
       int difference = hushEnd - currentTime;
+      Serial.println(difference);
       if (difference <= 0) {
         return false;
       } else {
@@ -222,14 +228,31 @@ class Controller {
     }
 
     boolean interpretData() { //Check to see if the smoke and CO readings are below the set maximums. If they exceed the set maximums, the alarm and lights will be triggered.
-      if ((currentReadings[0] > MAXSMOKE) && (currentReadings[1] > MAXCARBON) && !isSnoozed()) {
+//      if (((currentReadings[0] > MAXSMOKE) || (currentReadings[1] > MAXCARBON)) && !isSnoozed()) {
+//        if(readingsSaved >= 100 && (currentReadings[2] - averageReadings[2])
+//        activateAlarm();
+//        activateLights();
+//      }else {
+//        
+//      }
+
+      //If the current smoke reading is over the threshold AND the temperature has increased more than 3 degrees in the past minute
+      if((currentReadings[0] > MAXSMOKE) && (currentReadings[2] - averageReadings[2] > 3)){
         activateAlarm();
-        activateLights();
+        activateLights(); 
+      }else if(currentReadings[1] >= MAXCARBON){
+          activateAlarm();
+          activateLights();
+        }else{
+          deactivateAlarm();
+          deactivateLights();
+        }
       }
-    }
+      //If the CO reading is over the threshold
+    
 
     void hushAlarm() { //Disables the alarm and lights, sets HushEnd to SNOOZETIME milliseconds in the future
-      hushEnd = time(0);
+      hushEnd = now();
       hushEnd = hushEnd + SNOOZETIME;
       deactivateAlarm();
       deactivateLights();
@@ -239,7 +262,9 @@ class Controller {
       while (true) {
         getSensorData();
         logData();
+        if((readingsSaved % 60) == 0){
         interpretData();
+        }
         if (Serial.available()) {
           Serial.print("Smoke Sensor: ");
           Serial.print(currentReadings[0]);
@@ -248,11 +273,14 @@ class Controller {
           Serial.print("\tTemperature: ");
           Serial.println(currentReadings[2]);
         }
-        delay(10);
+        if(sirenActivated){
+          sc.activate();
+        }
+        delay(500);//This makes it read about once per second
       }
     }
     Controller() {
-      hushEnd = time(0);
+      hushEnd = now();
     }
 };
 void setup() {
