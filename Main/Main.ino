5ca1218de137f9e38091749b54b2232e47233d6e
Jared Murphy <murphy.248@wright.edu>
2019-11-07 09:38:10 -0500

    Merged PrototypeThree and HostServer

--- Main/Main.ino
+++ Main/Main.ino
@@ -26,7 +26,7 @@ WiFiServer server(80);
 // constants
 
 #define MAXSMOKE 150
-#define MAXCARBON 0//800
+#define MAXCARBON 400//800
 #define SNOOZETIME 300 //time in seconds, 5 minutes
 #define MAXREADINGS 32768
 
@@ -64,9 +64,6 @@ class LEDController {
       Serial.println("Turning LED off");
       return true;
     }
-
-    
-
 };
 
 class SirenController {
@@ -116,7 +113,7 @@ class TempSensor {
   public:
     double getReading() {
       sensors.requestTemperatures();
-      return ((sensors.getTempCByIndex(0) * 9.0) / 5.0 + 32.0);      
+      return ((sensors.getTempCByIndex(0) * 9.0) / 5.0 + 32.0);
     }
 
 };
@@ -162,140 +159,37 @@ class CarbonDetector {
       }
     }
 };
-
-    //////////////////////////////////////////////////////Server/////////////////////////////////////
-    class HostServer{
-      
-       void SetupServer() {
-        Serial.begin(9600);      // initialize serial communication
-      
-        // check for the WiFi module:
-        if (WiFi.status() == WL_NO_MODULE) {
-          Serial.println("Communication with WiFi module failed!");
-          // don't continue
-          while (true);
-        }
-      
-        String fv = WiFi.firmwareVersion();
-        if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-          Serial.println("Please upgrade the firmware");
-        }
-      
-        // attempt to connect to Wifi network:
-        while (status != WL_CONNECTED) {
-          Serial.print("Attempting to connect to Network named: ");
-          Serial.println(ssid);                   // print the network name (SSID);
-      
-          // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-          status = WiFi.begin(ssid);
-          // wait 10 seconds for connection:
-          delay(2000);
-        }
-        server.begin();                           // start the web server on port 80
-        //printWifiStatus();                        // you're connected now, so print out the status
-      }
-      
-      
-      
-      void UpdateHTML() {
-        WiFiClient client = server.available();   // listen for incoming clients
-      
-        if (client) {                             // if you get a client,
-          Serial.println("new client");           // print a message out the serial port
-          String currentLine = "";                // make a String to hold incoming data from the client
-          while (client.connected()) {            // loop while the client's connected
-            if (client.available()) {             // if there's bytes to read from the client,
-              char c = client.read();             // read a byte, then
-              //Serial.write(c);                    // print it out the serial monitor
-              if (c == '\n') {                    // if the byte is a newline character
-      
-                // if the current line is blank, you got two newline characters in a row.
-                // that's the end of the client HTTP request, so send a response:
-                if (currentLine.length() == 0) {
-                  // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-                  // and a content-type so the client knows what's coming, then a blank line:
-                  client.println("HTTP/1.1 200 OK");
-                  client.println("Content-type:text/html");
-                  client.println("Connection: close");
-                  client.println(); // this is needed
-      
-      ppp("<!DOCTYPE html>");
-        ppp("<head>");
-          ppp("<style>body {background-color:#00e7ff52;} a {color:orange;}</style>");
-          ppp("<style>div {border-style: groove; padding: 10px; } </style>");
-          ppp("<style>button {padding: 20px; width: 200px; } </style>");
-          ppp("<style>p {padding: 10px; display: block; white-space: pre; font-family: Consolas;} </style>");
-        ppp("</head>");
-        ppp("<body>");
-          ppp("<div>");
-            ppp("<button type=\"button\" onclick=\"window.location.href = \'/toggleSpotlight\'\"> Turn spotlight " + cont.getOppositeLEDState() + "</button><br>");
-            //ppp("Click <a href=\"/H\">here</a> turn the LED on pin 9 on<br>");
-            ppp("<button type=\"button\" onclick=\"window.location.href = \'/toggleAlarm\'\"> Turn alarm " + cont.getOppositeSirenState() + "</button><br>");
-            
-          ppp("</div>");
-          ppp("<div>");
-            ppp("<p>Temerature:         "+ String(cont.currentReading[2], DEC) +" &#176;C</p>");
-            ppp("<p>Carbon Monoxide:    "+ String(cont.currentReading[1], DEC) +" parts per million</p>");
-            ppp("<p>Smoke:              "+ String(cont.currentReading[0], DEC) +" parts per million</p>");
-          ppp("</div>");
-      
-        ppp("</body>");
-      ppp("</html>");
-      // 
-
-                  client.println(); // The HTTP response ends with another blank line:
-                  break;
-                } else {    // if you got a newline, then clear currentLine:
-                  currentLine = "";
-                }
-              } else if (c != '\r') {  // if you got anything else but a carriage return character,
-                currentLine += c;      // add it to the end of the currentLine
-              }
-              if (currentLine.endsWith("GET /toggleSpotlight")) {
-                cont.toggleSpotlight();
-              }
-              else if (currentLine.endsWith("GET /toggleAlarm")) {
-                cont.toggleAlarm();
-              }
-              
-      
-              
-            }
-          }
-          // close the connection:
-          client.stop();
-          Serial.println("client disonnected");
-        }
-      }
-
-      
-    };
-
 ///////////////////////////////////////////////////Controller//////////////////////////////////////////
-public:
 class Controller {
     SmokeDetector sd;
     CarbonDetector cd;
     TempSensor ts;
+    public:
     float currentReadings[3];
+    public:
     float averageReadings[3];
     int readingsSaved;
     LEDController lc;
     SirenController sc;
-    
+
     time_t hushEnd = now();
 
     bool lightsActivated = false;
     bool sirenActivated = false;
     bool online;
 
+  public:
+    Controller() {
+      hushEnd = now();
+    }
+    
     void getSensorData() { //Collect readings from the sensors.
       currentReadings[0] = sd.getReading();
 
       //Code for the CO2 sensor reading
       if (readingsSaved % (MAXREADINGS - 20) == 0) {
         currentReadings[1] = cd.getReading();
-      } else if (readingsSaved % (2* MAXREADINGS / 3) == 0) {
+      } else if (readingsSaved % (2 * MAXREADINGS / 3) == 0) {
         cd.warmUpTwo();
       } else if (readingsSaved % (MAXREADINGS / 3) == 0) {
         cd.warmUpOne();
@@ -324,14 +218,10 @@ class Controller {
       }
     }
 
-
-
-
-
     /// lights / LED ///
 
-     void toggleLights() {
-      if(lightsActivated == true){
+    void toggleLights() {
+      if (lightsActivated == true) {
         deactivateLights();
       } else
       {
@@ -349,8 +239,8 @@ class Controller {
       Serial.print("Deactivating lights!");
     }
 
-    String getOppositeLEDState(){
-      if(lightsActivated == true){
+    String getOppositeLEDState() {
+      if (lightsActivated == true) {
         return "off";
       } else
       {
@@ -361,7 +251,7 @@ class Controller {
     /// siren / alarm ///
 
     void toggleAlarm() {
-      if(sirenActivated == true){
+      if (sirenActivated == true) {
         deactivateAlarm();
         Serial.println("Turning alarm off");
       } else
@@ -379,8 +269,8 @@ class Controller {
       sirenActivated = false;
     }
 
-    String getOppositeSirenState(){
-      if(sirenActivated == true){
+    String getOppositeSirenState() {
+      if (sirenActivated == true) {
         return "off";
       } else
       {
@@ -401,28 +291,28 @@ class Controller {
     }
 
     boolean interpretData() { //Check to see if the smoke and CO readings are below the set maximums. If they exceed the set maximums, the alarm and lights will be triggered.
-//      if (((currentReadings[0] > MAXSMOKE) || (currentReadings[1] > MAXCARBON)) && !isSnoozed()) {
-//        if(readingsSaved >= 100 && (currentReadings[2] - averageReadings[2])
-//        activateAlarm();
-//        activateLights();
-//      }else {
-//        
-//      }
+      //      if (((currentReadings[0] > MAXSMOKE) || (currentReadings[1] > MAXCARBON)) && !isSnoozed()) {
+      //        if(readingsSaved >= 100 && (currentReadings[2] - averageReadings[2])
+      //        activateAlarm();
+      //        activateLights();
+      //      }else {
+      //
+      //      }
 
       //If the current smoke reading is over the threshold AND the temperature has increased more than 3 degrees in the past minute
-      if((currentReadings[0] > MAXSMOKE) && (currentReadings[2] - averageReadings[2] > 3)){
+      if ((currentReadings[0] > MAXSMOKE) && (currentReadings[2] - averageReadings[2] > 3)) {
         activateAlarm();
-        activateLights(); 
-      }else if(currentReadings[1] >= MAXCARBON){
-          activateAlarm();
-          activateLights();
-        }else{
-          deactivateAlarm();
-          deactivateLights();
-        }
+        activateLights();
+      } else if (currentReadings[1] >= MAXCARBON) {
+        activateAlarm();
+        activateLights();
+      } else {
+        deactivateAlarm();
+        deactivateLights();
       }
-      //If the CO reading is over the threshold
-    
+    }
+    //If the CO reading is over the threshold
+
 
     void hushAlarm() { //Disables the alarm and lights, sets HushEnd to SNOOZETIME milliseconds in the future
       hushEnd = now();
@@ -430,10 +320,9 @@ class Controller {
       deactivateAlarm();
       deactivateLights();
     }
-  public:
+    
     void loop() {
-
-        // called in loop of main
+      while (true) {
         getSensorData();
         logData();
         if((readingsSaved % 60) == 0){
@@ -450,23 +339,128 @@ class Controller {
         if(sirenActivated){
           sc.activate();
         }
-        
-        }
+        delay(500);//This makes it read about once per second
+      }
     }
-    
-    Controller() {
-      hushEnd = now();
-    }
+
 };
 
-void setup() {
-  
-  Controller cont;
-  HostServer hs;
-  hs.SetupServer(cont);
-  cont.loop();
+//////////////////////////////////////////////////////Server/////////////////////////////////////
+class HostServer {
+    Controller *controller;
+    public:
+    void SetupServer(Controller *cont) {
+      Serial.begin(9600);      // initialize serial communication
+      controller = cont;
+      // check for the WiFi module:
+      if (WiFi.status() == WL_NO_MODULE) {
+        Serial.println("Communication with WiFi module failed!");
+        // don't continue
+        while (true);
+      }
 
+      String fv = WiFi.firmwareVersion();
+      if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+        Serial.println("Please upgrade the firmware");
+      }
+
+      // attempt to connect to Wifi network:
+      while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to Network named: ");
+        Serial.println(ssid);                   // print the network name (SSID);
+
+        // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+        status = WiFi.begin(ssid);
+        // wait 10 seconds for connection:
+        delay(2000);
+      }
+      server.begin();                           // start the web server on port 80
+      //printWifiStatus();                        // you're connected now, so print out the status
+    }
+
+
+
+    void UpdateHTML() {
+      WiFiClient client = server.available();   // listen for incoming clients
+
+      if (client) {                             // if you get a client,
+        Serial.println("new client");           // print a message out the serial port
+        String currentLine = "";                // make a String to hold incoming data from the client
+        while (client.connected()) {            // loop while the client's connected
+          if (client.available()) {             // if there's bytes to read from the client,
+            char c = client.read();             // read a byte, then
+            //Serial.write(c);                    // print it out the serial monitor
+            if (c == '\n') {                    // if the byte is a newline character
+
+              // if the current line is blank, you got two newline characters in a row.
+              // that's the end of the client HTTP request, so send a response:
+              if (currentLine.length() == 0) {
+                // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                // and a content-type so the client knows what's coming, then a blank line:
+                client.println("HTTP/1.1 200 OK");
+                client.println("Content-type:text/html");
+                client.println("Connection: close");
+                client.println(); // this is needed
+
+                ppp("<!DOCTYPE html>");
+                ppp("<head>");
+                ppp("<style>body {background-color:#00e7ff52;} a {color:orange;}</style>");
+                ppp("<style>div {border-style: groove; padding: 10px; } </style>");
+                ppp("<style>button {padding: 20px; width: 200px; } </style>");
+                ppp("<style>p {padding: 10px; display: block; white-space: pre; font-family: Consolas;} </style>");
+                ppp("</head>");
+                ppp("<body>");
+                ppp("<div>");
+                ppp("<button type=\"button\" onclick=\"window.location.href = \'/toggleSpotlight\'\"> Turn spotlight " + controller->getOppositeLEDState() + "</button><br>");
+                //ppp("Click <a href=\"/H\">here</a> turn the LED on pin 9 on<br>");
+                ppp("<button type=\"button\" onclick=\"window.location.href = \'/toggleAlarm\'\"> Turn alarm " + controller->getOppositeSirenState() + "</button><br>");
+
+                ppp("</div>");
+                ppp("<div>");
+                ppp("<p>Temerature:         " + String(controller->currentReadings[2], DEC) + " &#176;C</p>");
+                ppp("<p>Carbon Monoxide:    " + String(controller->currentReadings[1], DEC) + " parts per million</p>");
+                ppp("<p>Smoke:              " + String(controller->currentReadings[0], DEC) + " parts per million</p>");
+                ppp("</div>");
+
+                ppp("</body>");
+                ppp("</html>");
+                //
+
+                client.println(); // The HTTP response ends with another blank line:
+                break;
+              } else {    // if you got a newline, then clear currentLine:
+                currentLine = "";
+              }
+            } else if (c != '\r') {  // if you got anything else but a carriage return character,
+              currentLine += c;      // add it to the end of the currentLine
+            }
+            if (currentLine.endsWith("GET /toggleSpotlight")) {
+              controller->toggleLights();
+            }
+            else if (currentLine.endsWith("GET /toggleAlarm")) {
+              controller->toggleAlarm();
+            }
+
+          }
+        }
+        // close the connection:
+        client.stop();
+        Serial.println("client disonnected");
+      }
+    }
+
+
+};
+////////////////////////////Setup & Loop////////////////////////
+
+void setup() {
+  Controller c;
+  HostServer hs;
+  hs.SetupServer(&c);
+  c.loop();
   hs.UpdateHTML();
   delay(500);//This makes it read about once per second
+}
+void loop(){
 
 }
